a2abfec4c8eff9ad3d15b99e3db63349f188e9a1
Julia <dawid@DESKTOP-1BH4PB6>
2026-03-12 14:51:07 +0100

    listbox

--- labolatorium2/wybierz.cs
+++ labolatorium2/wybierz.cs
@@ -15,11 +15,16 @@ namespace labolatorium2
         public wybierz()
         {
             InitializeComponent();
+            listBox1.Items.Add("Pizza - 25 zł");
+            listBox1.Items.Add("Burger - 18 zł");
+            listBox1.Items.Add("Frytki - 10 zł");
+            listBox1.Items.Add("Kebab - 22 zł");
+            listBox1.Items.Add("Cola - 7 zł");
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
